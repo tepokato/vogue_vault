d1720a27bae86310b9919b6775d84acaa8c82dda
tepokato <tepokato@gmail.com>
2025-08-12 22:11:28 -0500

    Test legacy appointment provider handling

---
+++ test/services/appointment_service_test.dart
@@ -0,0 +1,71 @@
+import 'dart:io';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:path_provider_platform_interface/path_provider_platform_interface.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+import 'package:vogue_vault/services/appointment_service.dart';
+import 'package:vogue_vault/models/service_type.dart';
+import 'package:vogue_vault/models/user_profile.dart';
+
+class _FakePathProviderPlatform extends PathProviderPlatform {
+  @override
+  Future<String?> getApplicationDocumentsPath() async => Directory.systemTemp.path;
+
+  @override
+  Future<String?> getApplicationSupportPath() async => Directory.systemTemp.path;
+
+  @override
+  Future<String?> getTemporaryPath() async => Directory.systemTemp.path;
+}
+
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUpAll(() {
+    PathProviderPlatform.instance = _FakePathProviderPlatform();
+  });
+
+  tearDown(() async {
+    await Hive.deleteFromDisk();
+  });
+
+  test('legacy appointments load with default providerId', () async {
+    final service = AppointmentService();
+    await service.init();
+
+    final box = Hive.box<Map<String, dynamic>>('appointments');
+    await box.put('a1', {
+      'id': 'a1',
+      'clientId': 'c1',
+      'service': ServiceType.hairCut.name,
+      'dateTime': DateTime.parse('2023-01-01').toIso8601String(),
+    });
+
+    final appts = service.appointments;
+    expect(appts, hasLength(1));
+    expect(appts.first.providerId, '');
+  });
+
+  test('deleteUser handles legacy appointments', () async {
+    final service = AppointmentService();
+    await service.init();
+
+    final apptsBox = Hive.box<Map<String, dynamic>>('appointments');
+    final usersBox = Hive.box<Map<String, dynamic>>('users');
+
+    await usersBox.put('c1', UserProfile(id: 'c1', name: 'Client').toMap());
+
+    await apptsBox.put('a1', {
+      'id': 'a1',
+      'clientId': 'c1',
+      'service': ServiceType.hairCut.name,
+      'dateTime': DateTime.parse('2023-01-01').toIso8601String(),
+    });
+
+    await service.deleteUser('c1');
+
+    expect(apptsBox.isEmpty, isTrue);
+    expect(usersBox.get('c1'), isNull);
+  });
+}
